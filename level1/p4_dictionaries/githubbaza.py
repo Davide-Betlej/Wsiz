fad9bde240f922546f04bbd938bbdd9b98615197
Davide Betlej <davidebetlej1@gmail.com>
2020-11-07 00:18:06 +0100

    Dodano zadanie z githubowej bazy danych

--- level1/p4_dictionaries/githubbaza.py
+++ level1/p4_dictionaries/githubbaza.py
@@ -1,29 +1,21 @@
 import requests
 from time import sleep
 
+unikalni1= {}
 def recent_logins():
     # zbiera loginname userów 30 ostatnich zdarzeń na github
     r = requests.get('https://api.github.com/events')
     j = r.json()  # to jest tablica słowników/dict
     users = []
+    unikalni= {}
     for e in j:
         users.append(e['actor']['login'])
-    return users
+        unikalni[e['actor']['login']] = 1
+    return unikalni
 
 for i in range(12):
-    users = recent_logins()
-    users.append(users)
-    print(users)
+    unikalni1.update(recent_logins())
+    print(unikalni1)
     sleep(10)
 
-unikalna_lista = []
-for item in users:
-    if item not in unikalna_lista:
-        unikalna_lista.append(item)
-
-print('Unikalne loginy pomyślnie zapisane w pliku loginy.txt')
-print(f'Liczba unikalnych loginów:{len(unikalna_lista)}')
-
-plik1 = open('loginy.txt', 'a', encoding='UTF-8')
-plik1.write(f'Twoja unikalna lista to: {item}')
-plik1.close
+print(unikalni1.keys())
